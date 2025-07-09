6abeb98b70d13243d9f7b94b7a248f4a29ff5b21
Edward J. Garcia <123278174+DevOpulent@users.noreply.github.com>
2025-07-08 20:46:31 -0500

    Updates

---
+++ precache-manifest.98c102b1f10bd06666fab544f9a0ae1b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ceee216c4f873a373d4b0424344f2c2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a9e5ca7f60af265e95de",
+    "url": "/static/css/2.02e251ca.chunk.css"
+  },
+  {
+    "revision": "9a23042e0522de00b1d6",
+    "url": "/static/css/main.8a84ef2d.chunk.css"
+  },
+  {
+    "revision": "a9e5ca7f60af265e95de",
+    "url": "/static/js/2.b396919f.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/static/js/2.b396919f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a23042e0522de00b1d6",
+    "url": "/static/js/main.c2c8cfd5.chunk.js"
+  },
+  {
+    "revision": "6074ff583ca711c31fe3",
+    "url": "/static/js/runtime-main.1dbfc099.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.478dd7aeac4fa3e96dbe7e5d46a5c126.js"
+  "/precache-manifest.98c102b1f10bd06666fab544f9a0ae1b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
