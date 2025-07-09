b43f1626b293fa428aa7cb6def8ae33024539dbe
Edward J. Garcia <123278174+DevOpulent@users.noreply.github.com>
2025-07-08 20:40:12 -0500

    Updates

---
+++ precache-manifest.478dd7aeac4fa3e96dbe7e5d46a5c126.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1de1785f3013148db5c3f422a7694c5",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a9e5ca7f60af265e95de",
+    "url": "./static/css/2.02e251ca.chunk.css"
+  },
+  {
+    "revision": "eb5836894b9e6207c3d7",
+    "url": "./static/css/main.8a84ef2d.chunk.css"
+  },
+  {
+    "revision": "a9e5ca7f60af265e95de",
+    "url": "./static/js/2.b396919f.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "./static/js/2.b396919f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb5836894b9e6207c3d7",
+    "url": "./static/js/main.23f7da43.chunk.js"
+  },
+  {
+    "revision": "b3287891d2c1f1cb6915",
+    "url": "./static/js/runtime-main.590b0372.js"
+  }
+]);
